b3eca5946f3cc5a6ef919a9c75394b74c969876f
MarcinGa <marcin.gasiorek91@gmail.com>
2023-01-22 15:36:24 +0100

    cog rwd, finish

--- js/main.js
+++ js/main.js
@@ -1,27 +1,41 @@
-var type = 1, //circle type - 1 whole, 0.5 half, 0.25 quarter
-    radius = '298px', //distance from center
-    start = -90, //shift start from 0
-    $elements = $('.cog-ellipse__element'),
-    numberOfElements = (type === 1) ?  $elements.length : $elements.length - 1, //adj for even distro of elements when not full circle
-    slice = 360 * type / numberOfElements;
-
-$elements.each(function(i) {
-    var $self = $(this),
-        rotate = slice * i + start,
-        rotateReverse = rotate * -1;
-
-        if(rotate === -90) {
-            $self.find('figcaption').addClass('cog-ellipse__subtitle--top');
-        } else if (rotate > -90 && rotate < 90) {
-            $self.find('figcaption').addClass('cog-ellipse__subtitle--right');
-        } else if (rotate === 90) {
-            $self.find('figcaption').addClass('cog-ellipse__subtitle--bottom');
-        } else {
-            $self.find('figcaption').addClass('cog-ellipse__subtitle--left');
-        }
-        
-    
-    $self.css({
-        'transform': 'rotate(' + rotate + 'deg) translate(' + radius + ') rotate(' + rotateReverse + 'deg)'
-    });
+$(document).ready(function() {
+    adjustCog();
 });
+
+$(window).resize(function() {
+    adjustCog();
+})
+
+function adjustCog() {
+    var type = 1,
+        radius = '298px',
+        start = -90,
+        $elements = $('.cog-ellipse-element'),
+        numberOfElements = $elements.length,
+        slice = 360 * type / numberOfElements;
+
+        if($(window).width() <= 1280) {
+            radius = '220px';
+        }
+
+    $elements.each(function(i) {
+        var $self = $(this),
+            rotate = slice * i + start,
+            rotateReverse = rotate * -1;
+
+            if(rotate === -90) {
+                $self.find('figcaption').addClass('cog-ellipse-element__subtitle--top');
+            } else if (rotate > -90 && rotate < 90) {
+                $self.find('figcaption').addClass('cog-ellipse-element__subtitle--right');
+            } else if (rotate === 90) {
+                $self.find('figcaption').addClass('cog-ellipse-element__subtitle--bottom');
+            } else {
+                $self.find('figcaption').addClass('cog-ellipse-element__subtitle--left');
+            }
+            
+        
+        $self.css({
+            'transform': 'rotate(' + rotate + 'deg) translate(' + radius + ') rotate(' + rotateReverse + 'deg)'
+        });
+    });
+}
